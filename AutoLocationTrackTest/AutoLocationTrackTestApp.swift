b7a36adb8ad9719ac2652c420dc1837bc7cf26e7
Adrian <adrian.y.rachman@gmail.com>
2025-10-08 10:12:56 +0800

    Initial Commit

---
+++ AutoLocationTrackTest/AutoLocationTrackTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoLocationTrackTestApp.swift
+//  AutoLocationTrackTest
+//
+//  Created by Adrian Yusufa Rachman on 08/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AutoLocationTrackTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoLocationTrackTestTests/AutoLocationTrackTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutoLocationTrackTestTests.swift
+//  AutoLocationTrackTestTests
+//
+//  Created by Adrian Yusufa Rachman on 08/10/25.
+//
+
+import Testing
+@testable import AutoLocationTrackTest
+
+struct AutoLocationTrackTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
